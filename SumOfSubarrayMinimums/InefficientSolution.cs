1883e62f7367f07cd3f029be1672d8fad91ec2eb
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-26 17:51:00 +0200

    907. Sum of Subarray Minimums https://leetcode.com/problems/sum-of-subarray-minimums/submissions/

---
+++ SumOfSubarrayMinimums/InefficientSolution.cs
@@ -0,0 +1,46 @@
+﻿public class InefficientSolution
+{
+    private readonly Dictionary<(int, int), int> _dictionary = new Dictionary<(int, int), int>();
+    private long sum = 0;
+    public int SumSubarrayMins(int[] arr)
+    {
+        FindMinOfIntervals(arr, 0, arr.Length - 1);
+        return (int)(sum % (Math.Pow(10, 9) + 7));
+    }
+
+    private int FindMinOfIntervals(int[] arr, int l, int r)
+    {
+        if (_dictionary.TryGetValue((l, r), out int existingMin))
+        {
+            return existingMin;
+        }
+
+        if (l == r)
+        {
+            _dictionary.Add((l, r), arr[l]);
+            sum += arr[l];
+            //Display(arr, l, r, arr[l]);
+
+            return arr[l];
+        }
+
+        var leftMin = FindMinOfIntervals(arr, l, r - 1);
+        var rightMin = FindMinOfIntervals(arr, l + 1, r);
+        var min = leftMin <= rightMin ? leftMin : rightMin;
+        _dictionary.Add((l, r), arr[l]);
+        sum += min;
+        //Display(arr, l, r, min);
+
+        return min;
+    }
+
+    private void Display(int[] arr, int l, int r, int min)
+    {
+        for (int i = l; i <= r; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.Write("-> " + min);
+        Console.WriteLine();
+    }
+}
---
+++ SumOfSubarrayMinimums/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int SumSubarrayMins(int[] arr)
+    {
+        var stack = new Stack<int>();
+        long[] sum = new long[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            while (stack.Count > 0 && arr[stack.Peek()] > arr[i])
+            {
+                stack.Pop();
+            }
+
+            var j = stack.Count > 0 ? stack.Peek() : -1;
+            stack.Push(i);
+            sum[i] = (j >= 0 ? sum[j] : 0) + (i - j) * arr[i];
+        }
+        return (int)(sum.Sum() % (Math.Pow(10, 9) + 7));
+    }
+}
