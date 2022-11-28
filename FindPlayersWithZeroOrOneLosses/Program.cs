a11c7636021e1cbbebe76ddec3e205861c7c2ba5
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-28 09:55:29 +0200

    2225. Find Players With Zero or One Losses
    https://leetcode.com/problems/find-players-with-zero-or-one-losses/

---
+++ FindPlayersWithZeroOrOneLosses/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var sol = new Solution();
+sol.FindWinners(new int[][] { new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 3, 6 }, new int[] { 5, 6 }, new int[] { 5, 7 }, new int[] { 4, 5 }, new int[] { 4, 8 }, new int[] { 4, 9 }, new int[] { 10, 4 }, new int[] { 10, 9 } });
---
+++ FindPlayersWithZeroOrOneLosses/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public IList<IList<int>> FindWinners(int[][] matches)
+    {
+        var winners = new HashSet<int>();
+        var losers = new Dictionary<int, int>();
+
+        foreach (var match in matches)
+        {
+            if (losers.TryGetValue(match.First(), out _))
+                winners.Remove(match.First());
+            else
+                winners.Add(match.First());
+
+            if (losers.TryGetValue(match.Last(), out _))
+                losers[match.Last()]++;
+            else
+                losers.Add(match.Last(), 1);
+
+            winners.Remove(match.Last());
+        }
+
+        return new List<IList<int>>{  winners.OrderBy(x=>x).ToList(),
+            losers.Where(x=>x.Value==1).Select(x=>x.Key).OrderBy(x=>x).ToList() };
+    }
+}
