2d9e1d363c03566c7a2bb43a70dd90d8e6351ff0
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-28 09:55:54 +0200

    2225. Find Players With Zero or One Losses https://leetcode.com/problems/find-players-with-zero-or-one-losses/

--- FindPlayersWithZeroOrOneLosses/Program.cs
+++ FindPlayersWithZeroOrOneLosses/Program.cs
@@ -1,4 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * 2225. Find Players With Zero or One Losses
+ * https://leetcode.com/problems/find-players-with-zero-or-one-losses/
+ *
+ */
+
 var sol = new Solution();
 sol.FindWinners(new int[][] { new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 3, 6 }, new int[] { 5, 6 }, new int[] { 5, 7 }, new int[] { 4, 5 }, new int[] { 4, 8 }, new int[] { 4, 9 }, new int[] { 10, 4 }, new int[] { 10, 9 } });
