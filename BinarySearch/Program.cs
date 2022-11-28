91091fa5c78b0b92ccdfb2a481be3f9c00195dbc
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-28 14:13:10 +0200

    704. Binary Search https://leetcode.com/problems/binary-search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * 704. Binary Search
+ * https://leetcode.com/problems/binary-search
+ */
+
+var sol = new Solution();
+
+Console.WriteLine(sol.Search(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
---
+++ BinarySearch/Solution.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        return BinarySearch(nums, 0, nums.Length - 1, target);
+    }
+
+    private int BinarySearch(int[] nums, int l, int r, int target)
+    {
+        if (l > r)
+            return -1;
+        var mid = (l + r) / 2;
+
+        if (target == nums[mid])
+            return mid;
+        if (target < nums[mid])
+            return BinarySearch(nums, l, mid - 1, target);
+
+        return BinarySearch(nums, mid + 1, r, target);
+    }
+}
