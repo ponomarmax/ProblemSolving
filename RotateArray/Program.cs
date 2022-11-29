cd2693db3b1f468a7aa491c078de3931243ed351
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-29 17:09:15 +0200

    977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array

---
+++ RotateArray/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * 977. Squares of a Sorted Array
+ * https://leetcode.com/problems/squares-of-a-sorted-array
+ */
+var sol = new Solution();
+sol.Rotate(new int[] { -1, -100, 3, 99 }, 2);
---
+++ RotateArray/Solution.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 977. Squares of a Sorted Array
+ * https://leetcode.com/problems/squares-of-a-sorted-array
+ */
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        if (k % nums.Length == 0)
+            return;
+        int index = 0;
+        int firstIndex = 0;
+        int previousElem = nums[0];
+        int nextElem;
+        int nextIndex = (index + k) % nums.Length;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nextElem = nums[nextIndex];
+            nums[nextIndex] = previousElem;
+            index = nextIndex;
+            if (nextIndex == firstIndex)
+            {
+                nextIndex = (index + 1 + k) % nums.Length;
+                firstIndex++;
+                previousElem = nums[index + 1];
+            }
+            else
+            {
+                nextIndex = (index + k) % nums.Length;
+                previousElem = nextElem;
+            }
+
+        }
+    }
+}
