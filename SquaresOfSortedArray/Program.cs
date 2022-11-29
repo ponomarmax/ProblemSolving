7569cb62bcc6bd910ab13d927de671313bcae719
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-11-29 11:04:13 +0200

    977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array

---
+++ SquaresOfSortedArray/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * 977. Squares of a Sorted Array
+ * https://leetcode.com/problems/squares-of-a-sorted-array
+ */
+var sol = new Solution();
+sol.SortedSquares(new int[] { -7, -3, 2, 3, 11 });
---
+++ SquaresOfSortedArray/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var (j, k) = (0, nums.Length - 1);
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (Math.Abs(nums[j]) > Math.Abs(nums[k]))
+            {
+                result[i] = nums[j] * nums[j];
+                j++;
+            }
+            else
+            {
+                result[i] = nums[k] * nums[k];
+                k--;
+            }
+        }
+
+        return result;
+    }
+}
