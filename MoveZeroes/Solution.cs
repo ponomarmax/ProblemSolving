763748618659d7dc201f6c9a712a2ac9bfe42ca5
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-01 17:07:19 +0200

    283. Move Zeroes https://leetcode.com/problems/move-zeroes
    167. Two Sum II - Input Array Is Sorted
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted

---
+++ MoveZeroes/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int firstInRange = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                (nums[i], nums[firstInRange]) = (nums[firstInRange], nums[i]);
+                firstInRange++;
+            }
+        }
+    }
+}
---
+++ MoveZeroes/SolutionTwoSum.cs
@@ -0,0 +1,19 @@
+﻿public class SolutionTwoSum
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var (i, j) = (0, numbers.Length - 1);
+        while (true)
+        {
+            var sum = numbers[i] + numbers[j];
+            if (sum == target)
+                return new int[] { i + 1, j + 1 };
+            if (sum < target)
+            {
+                i++;
+                continue;
+            }
+            j--;
+        }
+    }
+}
