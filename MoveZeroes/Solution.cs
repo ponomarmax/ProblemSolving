d817e737ae80853041a08cb481680fde1927b783
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-01 17:57:19 +0200

    557. Reverse Words in a String III https://leetcode.com/problems/reverse-words-in-a-string-iii

--- MoveZeroes/Solution.cs
+++ MoveZeroes/Solution.cs
@@ -1,4 +1,4 @@
-﻿public class Solution
+﻿public partial class Solution
 {
     public void MoveZeroes(int[] nums)
     {
--- MoveZeroes/SolutionTwoSum.cs
+++ MoveZeroes/SolutionTwoSum.cs
@@ -31,3 +31,43 @@ public partial class Solution
         }
     }
 }
+
+public partial class Solution
+{
+    public string ReverseWords(string s)
+    {
+        if (s.Length < 2)
+        {
+            return s;
+        }
+        var res = new char[s.Length];
+        var (i, j, k) = (0, 1, 0);
+        while (j < s.Length)
+        {
+            if (s[j] == ' ' || j + 1 == s.Length)
+            {
+                k = j;
+                if (s[j] == ' ')
+                {
+                    res[j] = ' ';
+                    j--;
+                }
+                else
+                {
+                    k++;
+                }
+
+                while (i < k)
+                {
+                    res[i] = s[j];
+                    i++;
+                    j--;
+                }
+                i = j = ++k;
+                continue;
+            }
+            j++;
+        }
+        return new string(res);
+    }
+}
