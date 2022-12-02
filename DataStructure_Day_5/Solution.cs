315b745fde5d3f1a60b235c176c18345332b48bc
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-02 12:20:52 +0200

    74. Search a 2D Matrix https://leetcode.com/problems/search-a-2d-matrix

---
+++ DataStructure_Day_5/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        var (l, r) = (0, matrix.Length - 1);
+        while (l <= r)
+        {
+            var m = (l + r) / 2;
+            if (matrix[m][0] <= target)
+            {
+                if (target <= matrix[m][^1])
+                    return Array.BinarySearch(matrix[m], target) >= 0;
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+        return false;
+    }
+}
