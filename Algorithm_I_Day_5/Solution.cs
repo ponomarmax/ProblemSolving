85c81046d1cb84ad62f2dc30b3f7ff1194793291
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-02 12:36:13 +0200

    876. Middle of the Linked List https://leetcode.com/problems/middle-of-the-linked-list

---
+++ Algorithm_I_Day_5/Solution.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public partial class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        var current = head;
+        var middle = current;
+        var counter = 0;
+        while (current.next != null)
+        {
+            counter++;
+            current = current.next;
+            if(counter % 2 == 1)
+                middle= middle.next;
+
+        }
+       
+        return middle;
+    }
+}
+
