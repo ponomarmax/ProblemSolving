5a9dd2a49e891849c4e4972bbf7cd7dc3cdcd321
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-02 11:59:36 +0200

    121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock

--- DataStuctureDay_3_4/Program.cs
+++ DataStuctureDay_3_4/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
 Console.WriteLine("Hello, World!");
+
+var sol = new Solution();
+sol.Generate(5);
+sol.MatrixReshape(new int[][] { new int[] { 1, 2, 3, 4 } }, 2, 2);
--- DataStuctureDay_3_4/Solution.cs
+++ DataStuctureDay_3_4/Solution.cs
@@ -8,7 +8,7 @@
         {
             if (!dict.TryAdd(nums1[i], 1))
             {
-                dict[nums1[i]] ++;
+                dict[nums1[i]]++;
             }
         }
 
@@ -23,5 +23,71 @@
             }
         }
         return res.ToArray();
+    }
+}
+
+public partial class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int lastMinElem = prices.First();
+        var maxProfit = 0;
+        var sum = new int[prices.Length];
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] > lastMinElem)
+            {
+                var profit = prices[i] - lastMinElem;
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+            else
+            {
+                lastMinElem = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
+
+public partial class Solution
+{
+    public int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        if (r * c != mat.Length * mat[0].Length)
+            return mat;
+        var res = new int[r][];
+        for (int i = 0; i < r; i++)
+        {
+            res[i] = new int[c];
+        }
+        var n = r * c;
+        for (int i = 0; i < n; i++)
+        {
+            res[i / c][i % c] = mat[i / mat[0].Length][i % mat[0].Length];
+        }
+        return res;
+    }
+}
+
+public partial class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var res = new List<IList<int>>(numRows);
+        res.Add(new int[] { 1 });
+        if (numRows == 1) { return res; }
+        for (int i = 1; i < numRows; i++)
+        {
+            res.Add(new int[i + 1]);
+            res[i][0] = res[i][^1] = 1;
+            for (int j = 1; j <= i - 1; j++)
+            {
+                res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+            }
+        }
+        return res;
     }
 }
