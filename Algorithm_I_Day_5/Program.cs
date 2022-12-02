3724dad08d7b41d0bdd45cc4b68f507bb599443d
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-02 13:18:53 +0200

    19. Remove Nth Node From End of List https://leetcode.com/problems/remove-nth-node-from-end-of-list

--- Algorithm_I_Day_5/Program.cs
+++ Algorithm_I_Day_5/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var sol = new Solution();
+sol.RemoveNthFromEnd(new ListNode(1, new ListNode(2, new ListNode(3))), 3);
--- Algorithm_I_Day_5/Solution.cs
+++ Algorithm_I_Day_5/Solution.cs
@@ -22,12 +22,46 @@ public partial class Solution
         {
             counter++;
             current = current.next;
-            if(counter % 2 == 1)
-                middle= middle.next;
+            if (counter % 2 == 1)
+                middle = middle.next;
 
         }
-       
+
         return middle;
+    }
+}
+
+public partial class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        if (n == 1 && head.next == null)
+        {
+            return null;
+        }
+
+        var current = head;
+        var parentOfRemove = head;
+        var driverCounter = 0;
+        var pretendiToRemoveCounter = 0;
+        while (current.next != null)
+        {
+            current = current.next;
+            driverCounter++;
+            if (driverCounter - pretendiToRemoveCounter > n)
+            {
+                parentOfRemove = parentOfRemove.next;
+                pretendiToRemoveCounter++;
+            }
+        }
+        if (driverCounter - n == -1)
+        {
+            return head.next;
+        }
+
+        parentOfRemove.next = parentOfRemove.next?.next;
+
+        return head;
     }
 }
 
