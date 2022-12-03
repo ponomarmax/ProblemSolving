c19e5cb98a0025587bce1fba559ad8c46aae1c15
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-03 15:35:27 +0200

    3. Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters

---
+++ Algorithm_Day_6/Solution.cs
@@ -0,0 +1,28 @@
+﻿public partial class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var hash = new HashSet<char>();
+        var tailPointer = 0;
+        var temporalMax = 0;
+        var globalMax = 0;
+        int i = 0;
+        while (i < s.Length)
+        {
+            if (hash.Add(s[i]))
+            {
+                temporalMax++;
+                i++;
+                if (temporalMax > globalMax)
+                    globalMax = temporalMax;
+            }
+            else
+            {
+                hash.Remove(s[tailPointer]);
+                temporalMax--;
+                tailPointer++;
+            }
+        }
+        return globalMax;
+    }
+}
