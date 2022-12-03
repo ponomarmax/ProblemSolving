2dd87babae712b3cfa09c0b7fb52e5654293c0c3
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-03 14:43:54 +0200

    383. Ransom Note https://leetcode.com/problems/ransom-note

--- DataStructure_Day_6/Solution.cs
+++ DataStructure_Day_6/Solution.cs
@@ -14,3 +14,33 @@
         return min == s.Length ? -1 : min;
     }
 }
+
+public partial class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var countLetterInMagazine = new Dictionary<char, int>();
+        for (int i = 0; i < magazine.Length; i++)
+        {
+            if (!countLetterInMagazine.TryAdd(magazine[i], 1))
+            {
+                countLetterInMagazine[magazine[i]] += 1;
+            }
+        }
+
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            if (countLetterInMagazine.TryGetValue(ransomNote[i], out _))
+            {
+                countLetterInMagazine[ransomNote[i]] -= 1;
+                if (countLetterInMagazine[ransomNote[i]] < 0)
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
