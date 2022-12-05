064873a999cef450412da9ff05cee9535f50b258
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-05 17:43:32 +0200

    206. Reverse Linked List https://leetcode.com/problems/reverse-linked-list

---
+++ DS_D_8/Program.cs
@@ -0,0 +1,3 @@
+﻿var sol = new Solution();
+
+sol.ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
---
+++ DS_D_8/Solution.cs
@@ -0,0 +1,47 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+        var parentOfSlidePtr = head;
+        var slidePtr = head.next;
+        while(slidePtr != null)
+        {
+            var t = slidePtr.next;
+            slidePtr.next = parentOfSlidePtr;
+            parentOfSlidePtr = slidePtr;
+            slidePtr = t;
+        }
+        head.next = null;
+        
+        return parentOfSlidePtr;
+    }
+    //public ListNode ReverseList(ListNode head)
+    //{
+    //    if (head == null || head.next == null)
+    //        return head;
+    //    var r = ReverseList(head, head.next);
+    //    head.next = null;
+    //    return r;
+    //}
+
+    //private ListNode ReverseList(ListNode previous, ListNode current)
+    //{
+    //    ListNode previo = null;
+    //    if (current.next != null)
+    //        previo = ReverseList(current, current.next);
+    //    current.next = previous;
+    //    return previo ?? current;
+    //}
+}
