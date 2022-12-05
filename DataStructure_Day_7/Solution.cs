91b432233eeb89f9880dbadca59d5446b7f0038f
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-05 16:42:42 +0200

    141. Linked List Cycle https://leetcode.com/problems/linked-list-cycle
    21. Merge Two Sorted Lists
    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ DataStructure_Day_7/Solution.cs
@@ -0,0 +1,71 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+public partial class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var fast = head;
+        var slow = head;
+        while (true)
+        {
+            fast = fast?.next?.next;
+            slow = slow?.next;
+            if (fast == null)
+                return false;
+            if (slow == fast)
+                return true;
+        }
+    }
+}
+
+public partial class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode main = list1;
+        if (list1.val > list2.val)
+        {
+            main = list2;
+            list2 = list2.next;
+        }
+        else
+        {
+            list1 = list1.next;
+        }
+        var mainSlide = main;
+        while (list1 != null && list2 != null)
+        {
+            MakeStepInMergeSorting(ref mainSlide, ref (list1.val > list2.val) ? ref list2 : ref list1);
+        }
+        if (list1 != null)
+        {
+            MakeStepInMergeSorting(ref mainSlide, ref list1);
+        }
+        else if (list2 != null)
+        {
+            MakeStepInMergeSorting(ref mainSlide, ref list2);
+        }
+
+        return main;
+    }
+
+    private void MakeStepInMergeSorting(ref ListNode mainSlidePointer, ref ListNode listPointer)
+    {
+        mainSlidePointer.next = listPointer;
+        mainSlidePointer = mainSlidePointer.next;
+        listPointer = listPointer.next;
+    }
+}
+
