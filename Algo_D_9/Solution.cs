67b20b59cf90cd5f1db872c35ef0bf16a98ef437
Maksym Ponomarenko <mponomarenko@oneviewhealthcare.com>
2022-12-07 18:47:32 +0200

    994. Rotting Oranges
    https://leetcode.com/problems/rotting-oranges/

--- Algo_D_9/Solution.cs
+++ Algo_D_9/Solution.cs
@@ -1,4 +1,52 @@
-﻿public class Solution
+﻿public partial class Solution
+{
+    public int OrangesRotting(int[][] mat)
+    {
+        var neigbourPos = new int[] { 0, 1, 0, -1, 0 };
+        var rottedOranges = new List<(int, int)>();
+        var rottedHashOranges = new HashSet<(int, int)>();
+        var normalOranges = new HashSet<(int, int)>();
+        for (int i = 0; i < mat.Length; ++i)
+        {
+            for (int j = 0; j < mat[0].Length; ++j)
+            {
+                if (mat[i][j] == 2)
+                {
+                    rottedOranges.Add((i, j));
+                    rottedHashOranges.Add((i, j));
+                }
+                else if (mat[i][j] == 1)
+                    normalOranges.Add((i, j));
+            }
+        }
+        var offset = 0;
+        var counter = 0;
+        while (true)
+        {
+            var currentRottedOrangesAmount = rottedOranges.Count;
+            for (int i = offset; i < currentRottedOrangesAmount; ++i)
+            {
+                var rottedOrange = rottedOranges[i];
+                for (int j = 0; j < neigbourPos.Length - 1; ++j)
+                {
+                    var supposedPoint = (rottedOrange.Item1 + neigbourPos[j], rottedOrange.Item2 + neigbourPos[j + 1]);
+                    if (normalOranges.Remove(supposedPoint))
+                    {
+                        if (rottedHashOranges.Add(supposedPoint))
+                            rottedOranges.Add(supposedPoint);
+                    }
+                }
+            }
+            offset = currentRottedOrangesAmount;
+            if (offset < rottedOranges.Count) ++counter;
+            else break;
+        }
+
+        return normalOranges.Any() ? -1 : counter;
+    }
+}
+
+public partial class Solution
 {
     public int[][] UpdateMatrix(int[][] mat)
     {
